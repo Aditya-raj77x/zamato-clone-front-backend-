3046465fa42e453805774a47465ab8c15e8bee3d
Aditya-raj77x <ar8342077@gmail.com>
2022-10-05 12:53:27 -0400

    done added google authenthication

--- server/API/auth/index.js
+++ server/API/auth/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import passport from "passport";
 
 import { UserModel } from "../../database/allModels";
 import {
@@ -44,5 +45,27 @@ Router.post("/signin", async (req, res) => {
         return res.status(500).json({ error: error.message });
     }
 });
+
+Router.get(
+    "/google",
+    passport.authenticate("google", {
+        scope: [
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email",
+        ],
+    })
+);
+
+Router.get(
+    "/google/callback",
+    passport.authenticate("google", { failureRedirect: "/" }),
+    (req, res) => {
+
+
+        return res.redirect(
+            `http://localhost:3000/google/${req.session.passport.user.token}`
+        );
+    }
+);
 
 export default Router;
