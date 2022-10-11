31320f25c6e3dc55afcebc3ebacca14bf4c70d2a
Aditya-raj77x <ar8342077@gmail.com>
2022-10-11 15:13:59 -0400

    completed fodd and category card

--- client/src/pages/Home.page.jsx
+++ client/src/pages/Home.page.jsx
@@ -5,10 +5,10 @@ import { useParams } from "react-router-dom";
 import HomeLayout from "../layouts/Homepage.layout";
 
 // components
-// import Delivery from "../components/Delivery";
-// import Dining from "../components/Dining";
-// import NightLife from "../components/NightLife";
-// // import Nutrition from "../components/Nutrition";
+import Delivery from "../components/Delivery";
+import Dining from "../components/Dining";
+import NightLife from "../components/NightLife";
+import Nutrition from "../components/Nutrition";
 
 const Home = () => {
     const { type } = useParams();
@@ -16,10 +16,10 @@ const Home = () => {
     return (
         <>
             <div className="my-5">
-                {/* {type === "delivery" && <Delivery />}
+                {type === "delivery" && <Delivery />}
                 {type === "dining" && <Dining />}
                 {type === "night" && <NightLife />}
-                {type === "nutri" && <Nutrition />} */}
+                {type === "nutri" && <Nutrition />}
             </div>
         </>
     );
